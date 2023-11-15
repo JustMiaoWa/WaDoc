ae153d89c45ea38ed843550867d5be87d110b92f
JustMiaoWa <1421372365@qq.com>
2023-11-15 21:51:02 +0800

    update doc

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+// .vuepress/config.js
+module.exports = {
+    themeConfig: {
+      logo: '/img/top.png',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+    }
+}
